7fd40db01e30dcc4fd6cb1f054435189a4c02ff9
danieljbell <djbell70@gmail.com>
2017-07-19 19:41:39 -0400

    styles started

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,38 @@
-// var gulp          = require('gulp'),
-//     postcss       = require('gulp-postcss'),
-//     sass          = require('gulp-sass'),
-//     sourcemaps    = require('gulp-sourcemaps'),
-//     atImport      = require('postcss-import'),
-//     autoprefixer  = require('autoprefixer'),
-//     mqpacker      = require('css-mqpacker'),
-//     cssnano       = require('cssnano'),
-//     concat        = require('gulp-concat'),
-//     uglify        = require('gulp-uglify'),
-//     pump          = require('pump'),
-//     browserSync   = require('browser-sync');
+var gulp          = require('gulp'),
+    postcss       = require('gulp-postcss'),
+    sass          = require('gulp-sass'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    atImport      = require('postcss-import'),
+    autoprefixer  = require('autoprefixer'),
+    mqpacker      = require('css-mqpacker'),
+    cssnano       = require('cssnano'),
+    concat        = require('gulp-concat'),
+    uglify        = require('gulp-uglify'),
+    pump          = require('pump'),
+    browserSync   = require('browser-sync');
 
+gulp.task('css', function () {
+  var processors = [
+    atImport,
+    autoprefixer({browsers: ['last 6 versions']}),
+    mqpacker,
+    cssnano
+  ];
+  return gulp.src('_src/scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(postcss(processors))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('_src/scss/**/*.scss', ['css']);
+  // gulp.watch('src/js/app.js', ['js']);
+});
 
 
 // gulp.task('default', ['watch', 'browser-sync']);
+gulp.task('default', ['watch']);

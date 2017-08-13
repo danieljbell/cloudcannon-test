b4b83d7d22e184dc6dc37b5ba1af048f9a4c3a10
danieljbell <djbell70@gmail.com>
2017-08-13 15:59:58 -0400

    adding favicon and wrapping up homepage v1

--- _src/js/home.js
+++ _src/js/home.js
@@ -1,42 +1,53 @@
-// var airplaneContainer = document.querySelector('#airplane-container');
-// var airplaneImage = airplaneContainer.querySelector('img');
-// var airplaneOffset = airplaneContainer.offsetTop;
-// var airplaneHeight = airplaneContainer.offsetHeight;
-// var scrollTop = window.scrollY;
+var airplaneSection = document.querySelector('#airplane-section');
+var airplaneContainer = document.querySelector('#airplane-container');
+var airplaneImg = airplaneContainer.querySelector('img');
+var airplaneOffset = airplaneContainer.offsetTop;
 
+window.addEventListener('scroll', function() {
+  var scrollY = window.scrollY;
 
-var airplaneEntryWaypoint = new Waypoint({
-  element: document.querySelector('#airplane-container'),
-  handler: function(direction) {
-    document.body.classList.add('airplane-in-view');
-    document.body.classList.remove('airplane-out-view');
-  },
-  offset: '50%'
+  if (scrollY > (airplaneSection.offsetTop - 1000)) {
+    console.log('trigger');
+    airplaneImg.style.transform = 'translate(' + ((scrollY - airplaneSection.offsetTop + 200) / 2) + 'px, -' + ((scrollY - airplaneSection.offsetTop + 200) / 2) + 'px)';
+  }
+
+  // if (scrollY < (airplaneSection.offsetTop - 1000)) {
+  //   airplaneImg.style.transform = 'translate(0, 0)';
+  // }
+
 });
 
-var airplaneExitWaypoint = new Waypoint({
-  element: document.querySelector('#airplane-container'),
-  handler: function(direction) {
-    document.body.classList.remove('airplane-in-view');
-    document.body.classList.add('airplane-out-view');
-  },
-  offset: '20%'
-});
 
-// var airplaneInView = new Waypoint.Inview({
+
+
+// var airplaneEntryWaypoint = new Waypoint({
 //   element: document.querySelector('#airplane-container'),
-//   enter: function(direction) {
-
-//   },
-//   entered: function(direction) {
-//     document.body.classList.add('airplane-in-view');
-//   },
-//   exit: function(direction) {
-//     if (direction === 'up') {
+//   handler: function(direction) {
+//     if (direction === 'down') {
+//       document.body.classList.add('airplane-in-view');
+//       document.body.classList.remove('airplane-out-view');
+//     } else {
+//       document.body.classList.remove('airplane-out-view');
 //       document.body.classList.remove('airplane-in-view');
 //     }
 //   },
-//   exited: function(direction) {
-//     console.log('Exited triggered with direction ' + direction);
-//   }
-// })
+//   offset: '45%'
+// });
+
+// var airplaneExitWaypoint = new Waypoint({
+//   element: document.querySelector('#airplane-container'),
+//   handler: function(direction) {
+//     document.body.classList.remove('airplane-in-view');
+//     document.body.classList.add('airplane-out-view');
+//   },
+//   offset: '15%'
+// });
+
+// var airplaneExitWaypoint = new Waypoint({
+//   element: document.querySelector('#airplane-container'),
+//   handler: function(direction) {
+//     document.body.classList.remove('airplane-in-view');
+//     document.body.classList.remove('airplane-out-view');
+//   },
+//   offset: '-50%'
+// });
